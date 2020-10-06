a2fb7b31746db610a555ab1e650db1dd6d298b36
RealVkram <ajayiadegbemi.com>
2020-10-06 19:48:47 +0100

    react natice build

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, ScrollView, SafeAreaView, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const { cyanBox, yellow, yellowBox, cyan, red, safeArea, redBox } = styles;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <SafeAreaView>
+        <View style={safeArea}>
+          <View style={cyanBox}>
+            <Text style={cyan}>Hello, React Native Cyan</Text>
+          </View>
+          <View style={yellowBox}>
+            <Text style={yellow}>Hello, React Native yellow</Text>
+          </View>
+          <View style={redBox}>
+            <Text style={red}>Hello, React Native Red</Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
+    marginTop: 30,
+  },
+  cyanBox: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'cyan',
+    margin: 10,
+    padding: 20,
+  },
+  yellowBox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'yellow',
+    margin: 10,
+    padding: 20,
+  },
+  redBox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red',
+    margin: 10,
+    padding: 20,
+  },
+  red: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  yellow: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'green',
+  },
+  cyan: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: 'white',
   },
 });
+export default App;
